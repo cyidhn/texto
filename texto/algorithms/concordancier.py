3ba38b9e69df51a9c03b9ecfff0af378d80e20d0
Jérémy DEMANGE <j@lachainemedias.com>
2021-04-16 01:07:02 +0200

    Update concordancier

--- texto/algorithms/concordancier.py
+++ texto/algorithms/concordancier.py
@@ -1,25 +1,32 @@
-from nltk.draw.dispersion import dispersion_plot
-import matplotlib.pyplot as plt
+# from nltk.draw.dispersion import dispersion_plot
+# import matplotlib.pyplot as plt
 from nltk.text import Text
 from nltk.probability import FreqDist
 import spacy
 
 class Concordancier():
+    """
+    Fonction pour générer des concordancier.
+    """
 
     def __init__(self, corpus="fr_core_news_md"):
-        """Construction..."""
+        """..."""
         self.nlp = spacy.load(corpus)
 
-    def run(self, texte):
-        doc = nlp(texte)
+    def run(self, texte, lem=False):
+        """..."""
+        doc = self.nlp(texte)
         token = []
         for tok in doc:
             if "\n" not in tok.lemma_:
-                token.append(tok.text.lower())
-                # tok.append(token.lemma_.lower())
+                if lem:
+                    tok.append(token.lemma_.lower())
+                else:
+                    token.append(tok.text.lower())
         self.txt = Text(token)
         return self.txt
 
     def view(self, requete):
+        """..."""
         return self.txt.concordance(requete)
 
